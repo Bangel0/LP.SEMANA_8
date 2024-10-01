1dc1c16b503b022da376ee9b317c0c1568552752
Brahian <angelbrahian026@gmail.com>
2024-09-30 19:04:59 -0500

    cambio 4

--- ejercicio2/src/main/java/com/ejercicio2/Main.java
+++ ejercicio2/src/main/java/com/ejercicio2/Main.java
@@ -1,7 +1,45 @@
 package com.ejercicio2;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-       
+        Scanner omega = new Scanner(System.in);
+        Random rand = new Random();
+        int ramdon = rand.nextInt(100);
+        System.out.println(ramdon);
+        int cambio;
+        int i = 0;
+
+        while (true) {
+            if (i != ramdon) {
+                i++;
+            }
+            System.out.println("Ingrese el número aleatorio: ");
+            cambio = omega.nextInt();
+            
+            if (cambio < ramdon) {
+
+                System.out.println("El número oculto es mayor ");
+            }
+
+            if (cambio > ramdon) {
+                System.out.println("El número oculto es menor ");
+            }
+
+            if (cambio == ramdon) {
+
+                System.out.println("Número correcto ");
+                System.out.println("Intentos realizados: "+i);
+                
+                break;
+                
+            }
+            
+        }
+
     }
+    
 }
